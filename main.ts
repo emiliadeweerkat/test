f7a3e1e793e504aec50e1de8cb2fc2f49cc63731
Emilia Fournier <93674507+emiliadeweerkat@users.noreply.github.com>
2021-11-03 19:08:21 +0100

    laten werken

--- main.ts
+++ main.ts
@@ -1,3 +1,48 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("lets walk!")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . # . .
+        `)
+    basic.showString("tip doe een elastiekje om je been en doe de microbit tegen je been aan")
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showString("error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+})
+input.onGesture(Gesture.ThreeG, function () {
+    stap += 1
+    basic.showString("amogus")
+    basic.showLeds(`
+        . . # # #
+        . # # . .
+        . # # # #
+        . # # # #
+        . . # . #
+        `)
+})
+let stap = 0
+basic.showLeds(`
+    # . . . #
+    # # # # #
+    # . # . #
+    # # # # #
+    # # # # #
+    `)
+basic.showString("welcome to cat bit!")
